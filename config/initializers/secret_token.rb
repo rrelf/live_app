427924048a8cf9bd84b71e4e6677cf68e783e303
rrelf <rabenzie1@gmail.com>
2013-06-11 20:58:31 -0500

    Change .gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveApp::Application.config.secret_token = '70184d653e863ae52e331f08804d314cab6aba56a0d14860cfaa833d7d04993d327711a9076838cf187a0477c6915d017e822e00b8387e92777af750729af399'
---
+++ vendor/bundle/gems/listen-1.2.0/lib/listen/version.rb
@@ -0,0 +1,3 @@
+module Listen
+  VERSION = '1.2.0'
+end
